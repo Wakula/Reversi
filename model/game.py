f3ceda93cb49b8b7872e9680efc0bd2edbaa7353
Oleksandr Myrhorodskyi <kalipso121221@gmail.com>
2020-10-03 16:48:43 +0300

    Fixed errors. Added get winner method.

--- model/game.py
+++ model/game.py
@@ -1,18 +1,36 @@
-import * from constants
+from constants import *
 
 class Game:
-    _field = []
-    _current_player = Player.Black
-    _side_length = 8
 
-    def __init__():
-        _init_field()
+    def __init__(self):
+        self.current_player = Player.BLACK
+        self._side_length = 8
+
+        self._init_field()
 
 
-    def _init_field():
-        _field = [[Cell.Empty] * _side_length for i in range(_side_length)]
+    def _init_field(self):
+        self.field = [[Cell.EMPTY] * self._side_length for i in range(self._side_length)]
 
-        half_length = _side_length / 2
+        half_length = self._side_length // 2
 
-        _field[half_length][half_length] = _field[half_length + 1][half_length + 1] = Cell.White
-        _field[half_length][half_length + 1] = _field[half_length + 1][half_length] = Cell.Black
+        self.field[half_length][half_length] = self.field[half_length + 1][half_length + 1] = Cell.WHITE
+        self.field[half_length][half_length + 1] = self.field[half_length + 1][half_length] = Cell.BLACK
+
+    def get_winner(self):
+        black_cells = 0
+        white_cells = 0
+
+        for row in self.field:
+            for cell in row:
+                if cell == Cell.WHITE:
+                    white_cells += 1
+                elif cell == Cell.BLACK:
+                    black_cells += 1
+        
+        if (black_cells > white_cells):
+            return Player.BLACK
+        elif (white_cells > black_cells):
+            return Player.WHITE
+        else:
+            return Player.NONE
