ad3d2848e5780d304218f09f1a5d6c95e67fc528
Oleksandr Myrhorodskyi <kalipso121221@gmail.com>
2020-10-24 20:34:06 +0300

    Changed game start to fit test requirements.

--- play_reversi.py
+++ play_reversi.py
@@ -1,7 +1,5 @@
 from controller.game_controller import GameController
 
 end_game = False
-while not end_game:
-    game = GameController()
-    game.run_game()
-    end_game = game.end_game()
+game = GameController()
+game.run_game()
